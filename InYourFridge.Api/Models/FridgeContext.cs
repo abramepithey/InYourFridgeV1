104c478701dc4a9e5db9f48ec403ca1a7c5ef1a5
abramepithey <abramepithey@gmail.com>
2021-04-29 22:09:41 -0600

    Adds configurations and ModelBuilder specifics to the FridgeContext

--- InYourFridge.Api/Models/FridgeContext.cs
+++ InYourFridge.Api/Models/FridgeContext.cs
@@ -8,6 +8,17 @@ namespace InYourFridge.Api.Models
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=app.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ingredient>().HasMany(i => i.Orders).WithOne(o => o.Ingredient)
+                .HasForeignKey(o => o.IngredientId);
+        }
+
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<Order> Orders { get; set; }
     }

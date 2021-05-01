6e84c2e00f83df39fb08a116c92b3533633ff6ac
abramepithey <abramepithey@gmail.com>
2021-05-01 12:24:29 -0600

    Adds migrations

---
+++ InYourFridge.Api/Migrations/20210501144632_invertsFluidApi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InYourFridge.Api.Migrations
+{
+    public partial class invertsFluidApi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

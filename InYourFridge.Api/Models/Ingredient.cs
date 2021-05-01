3ff103d2a068fea3d1b08eaf92b40025727d60db
abramepithey <abramepithey@gmail.com>
2021-05-01 09:18:07 -0600

    Instantiates the Orders navigation property on creation

--- InYourFridge.Api/Models/Ingredient.cs
+++ InYourFridge.Api/Models/Ingredient.cs
@@ -6,7 +6,7 @@ namespace InYourFridge.Api.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
-        public virtual List<Order> Orders { get; set; }
+
+        public virtual List<Order> Orders { get; set; } = new List<Order>();
     }
 }

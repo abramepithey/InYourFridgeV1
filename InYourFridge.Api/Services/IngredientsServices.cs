11af161fbd60d9344bbe0c6833399cc104dd17db
abramepithey <abramepithey@gmail.com>
2021-05-02 09:46:40 -0600

    Creates the IngredientsServices class

---
+++ InYourFridge.Api/Services/IngredientsServices.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InYourFridge.Api.Contracts;
+using InYourFridge.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InYourFridge.Api.Services
+{
+    public class IngredientsServices
+    {
+        
+    }
+}

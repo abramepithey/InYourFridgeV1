c6af3dd39e6aa2a97d62874bbf5c9630d8718942
abramepithey <abramepithey@gmail.com>
2021-05-02 18:00:03 -0600

    Creates base class for index page

---
+++ InYourFridge.Blazor/Pages/IndexBase.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InYourFridge.Api.Models;
+using InYourFridge.Blazor.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace InYourFridge.Blazor.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        
+    }
+}
